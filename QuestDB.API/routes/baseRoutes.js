441e99f07a2208dd961a1016f05a60c8b40f29b9
Thiago <tmrocha@gmail.com>
2018-02-20 22:22:54 -0300

    implementação de rota inválida da API.

--- QuestDB.API/routes/baseRoutes.js
+++ QuestDB.API/routes/baseRoutes.js
@@ -3,15 +3,7 @@ var express = require('express');
 
 module.exports = function (app) {
 
-  // app.use("/QuestDB.Interface/scripts", express.static(__dirname + "../../QuestDB.Interface/scripts"));
-
-  // app.use("/QuestDB.Interface/img", express.static(__dirname + "../../QuestDB.Interface/img"));
-
-  // app.use("/QuestDB.Interface/styles", express.static(__dirname + "../../QuestDB.Interface/styles"));
-
-  // app.use("/QuestDB.Interface/partials", express.static(__dirname + "../../QuestDB.Interface/partials"));
-
   app.all('/*', function (req, res) {
-    res.sendFile(path.resolve('QuestDB.Interface/views/main/Index.html'));
+    res.send({ success : false, message : 'rota inválida'});
   });
 }
